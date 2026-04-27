cf4f9c7b72b489179ba979c49e1d513d69e0a4d8
Mariam Masurashvili <mariamimasurashvili66@gmail.com>
2026-04-27 10:11:22 +0400

    Add homework functions

---
+++ functions10.js
@@ -0,0 +1,65 @@
+function maxNumber(m, n) {
+    if (m > n) {
+        return m;
+    } else if (n > m) {
+        return n;
+    } else {
+        return 0;
+    }
+}
+
+console.log(maxNumber(5, 5));
+function sumNumbers(a, b) {
+    return a + b;
+}
+
+console.log(sumNumbers(3, 7));
+function printMyName() {
+    console.log("Mariami Masurashvili");
+}
+printMyName();
+
+function getFullName(firstname, lastname) {
+    return firstname + " " + lastname;
+}
+
+console.log(getFullName("Mariami", "Masurashvili"));
+
+function multiplyUpToN(n) {
+    let result = 1;
+
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+
+    return result;
+}
+
+console.log(multiplyUpToN(3));
+
+const student = {
+    firstName: "Mariami",
+    lastName: "Masurashvili",
+    age: 20,
+    scores: [4, 7, 5, 3, 2],
+
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+console.log(student.fullName());
+
+function sumScores(scores) {
+    let sum = 0;
+
+    for (let i = 0; i < scores.length; i++) {
+        sum += scores[i];
+    }
+
+    return sum;
+}
+
+console.log(sumScores(student.scores));
+
+console.log(student.firstName + " - " + student.age);
